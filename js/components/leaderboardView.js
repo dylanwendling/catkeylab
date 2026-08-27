ee9102a165eade7844a7cff4c7bbea55608e5b77
dylanwendling <turquoisedylanwstar45@gmail.com>
2026-08-27 13:40:05 -0500

    feat: implement anonymous global leaderboard view component with profile customization and test navigation

--- js/components/leaderboardView.js
+++ js/components/leaderboardView.js
@@ -3,7 +3,7 @@
    ========================================================================== */
 
 import { t } from '../i18n.js';
-import { getAnonProfile, updateAnonHandle, randomizeAnonHandle, cycleAnonAvatar, getLeaderboard } from '../leaderboard.js';
+import { getAnonProfile, randomizeAnonHandle, cycleAnonAvatar, getLeaderboard } from '../leaderboard.js';
 
 let activeTestId = 'reaction-time-test';
 
