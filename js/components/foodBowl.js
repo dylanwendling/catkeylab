011233f9dd54a05dfa7337f0f783e343763ad8f7
dylanwendling <turquoisedylanwstar45@gmail.com>
2026-08-27 15:31:27 -0500

    feat: implement interactive Nibbles cat mascot component with petting, feeding, and tracking animations

--- js/components/foodBowl.js
+++ js/components/foodBowl.js
@@ -122,19 +122,3 @@ function checkCatFeedingProximity(isRelease = false) {
     }
   }
 }
-
-export function spawnFishCenter() {
-  if (activeFishEl) activeFishEl.remove();
-
-  fishX = (window.innerWidth / 2);
-  fishY = (window.innerHeight / 2);
-
-  activeFishEl = document.createElement('div');
-  activeFishEl.className = 'draggable-fish';
-  activeFishEl.innerHTML = '🐟';
-  document.body.appendChild(activeFishEl);
-
-  updateFishPosition();
-  playClickSound(900, 0.04);
-  checkCatFeedingProximity(true);
-}
